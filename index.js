33bd487a65461957d68e8478a300a30fffbff3da
Peter <xervipeter@gmail.com>
2025-09-13 22:26:09 +0300

    upgrade

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 function distanceFromHqInBlocks(someValue) {
-  return Math.abs(someValue - 42);
+  const hqLocation = 42;
+  return Math.abs(someValue - hqLocation);
 }
-
 function distanceFromHqInFeet(someValue) {
-  return distanceFromHqInBlocks(someValue) * 264;
+  const blocks = distanceFromHqInBlocks(someValue); // get the number of blocks
+  const feet = blocks * 264; // convert blocks to feet
+  return feet;
+}
+function distanceTravelledInFeet(start, destination) {
+  const blocksTravelled = Math.abs(destination - start);
+  return blocksTravelled * 264;
+}
+function distanceTravelledInFeet(start, destination) {
+  return Math.abs(destination - start) * 264;
 }
 
 function calculatesFarePrice(start, destination) {
@@ -11,15 +20,11 @@ function calculatesFarePrice(start, destination) {
 
   if (distance <= 400) {
     return 0;
-  } else if (distance <= 2000) {
+  } else if (distance > 400 && distance <= 2000) {
     return (distance - 400) * 0.02;
-  } else if (distance <= 2500) {
+  } else if (distance > 2000 && distance <= 2500) {
     return 25;
   } else {
     return "cannot travel that far";
   }
-}
-
-function distanceTravelledInFeet(start, destination) {
-  return Math.abs(destination - start) * 264;
 }
